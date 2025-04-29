e9163c10519f66ac612f90a9f9f6e3625fa70dac
pineapple-guo <epicParsec@outlook.com>
2025-04-29 09:45:54 +0800

    feat: 添加路由功能并创建首页和关于页面
    引入react-router-dom实现路由功能，并创建首页和关于页面组件，以支持多页面导航

---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const About: React.FC = () => {
+  return <h2>关于页面</h2>
+}
+
+export default About
---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from '@/pages/Home'
+import About from '@/pages/About'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+])
+
+const AppRoutes: React.FC = () => <RouterProvider router={router} />
+
+export default AppRoutes
